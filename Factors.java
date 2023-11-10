5708795f075ae07161f6daf305766eb9989f5366
HalimaAlshehhi <150218956+HalimaAlshehhi@users.noreply.github.com>
2023-11-10 14:05:18 +0400

    Add files via upload

---
+++ Factors.java
@@ -0,0 +1,16 @@
+
+public class Factors {
+
+	public static void main(String[] args) {
+		   int n = 24;
+	        System.out.print("Factors of " + n + ": ");
+
+	        for (int i = 1; i <= n; i++) {
+	            if (n % i == 0) {
+	                System.out.print(i + " ");
+	            }
+	        }
+
+	}
+
+}
