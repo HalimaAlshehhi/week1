ef6b253718df9f454bec9c65c02c0b9e3e770c37
HalimaAlshehhi <150218956+HalimaAlshehhi@users.noreply.github.com>
2023-11-10 13:56:00 +0400

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,16 @@
+
+public class Square {
+
+	public static void main(String[] args) {
+		int n = 10; 
+
+        double squareOfSum = Math.pow((n * (n + 1) / 2.0), 2);
+
+        double sumOfSquares = (n * (n + 1) * (2 * n + 1)) / 6.0;
+
+        double difference = squareOfSum - sumOfSquares;
+
+        System.out.println("The difference is: " + difference);
+    }
+
+	}
