15cccf75d4d7e6391edddf69259cf32dbef02a8c
HalimaAlshehhi <150218956+HalimaAlshehhi@users.noreply.github.com>
2023-11-10 14:10:58 +0400

    Update FactorialOfNum.java

--- FactorialOfNum.java
+++ FactorialOfNum.java
@@ -2,15 +2,31 @@
 public class FactorialOfNum {
 
 	public static void main(String[] args) {
-		   int n = 24;
-	        System.out.print("Factors of " + n + ": ");
+		   Scanner scanner = new Scanner(System.in);
 
-	        for (int i = 1; i <= n; i++) {
-	            if (n % i == 0) {
-	                System.out.print(i + " ");
-	            }
-	        }
-	    }
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        long factorial = calculateFactorial(number);
+
+        System.out.println("Factorial of " + number + " is: " + factorial);
+
+        scanner.close();
+    }
+
+    private static long calculateFactorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial is not defined for negative numbers");
+        }
+
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+
+	}
 
 	}
 
