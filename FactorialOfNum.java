ab9a91f33723fb5e0f6a25f33eb0e5b0bc1980d2
HalimaAlshehhi <150218956+HalimaAlshehhi@users.noreply.github.com>
2023-11-10 13:41:45 +0400

    Add files via upload

---
+++ FactorialOfNum.java
@@ -0,0 +1,16 @@
+
+public class FactorialOfNum {
+
+	public static void main(String[] args) {
+		   int n = 24;
+	        System.out.print("Factors of " + n + ": ");
+
+	        for (int i = 1; i <= n; i++) {
+	            if (n % i == 0) {
+	                System.out.print(i + " ");
+	            }
+	        }
+	    }
+
+	}
+
